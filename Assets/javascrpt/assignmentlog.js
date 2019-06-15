00018e4a9306e5d3291774ca12129f4d481c775a
Daniel Jackson <officialdanieljackson@gmail.com>
2019-06-15 02:19:41 -0700

    adding files

--- Assets/javascrpt/assignmentlog.js
+++ Assets/javascrpt/assignmentlog.js
@@ -1,61 +1,69 @@
 $(document).ready(function() {
-    console.log(“hello world”);
-
-    // Your web app's Firebase configuration
+  console.log("hello world");
+  
   var firebaseConfig = {
-    apiKey: "AIzaSyAgC2DGeeFWV8C61Rcmrv8IdlCawDW1kzw",
-    authDomain: "first-group-project-f0582.firebaseapp.com",
-    databaseURL: "https://first-group-project-f0582.firebaseio.com",
-    projectId: "first-group-project-f0582",
-    storageBucket: "first-group-project-f0582.appspot.com",
-    messagingSenderId: "184202572556",
-    appId: "1:184202572556:web:4c98835478d1e3d9"
-  };
-   
+      apiKey: "AIzaSyD1h4QrWTz6J0X3SJDa0kB69g3atX17PmU",
+      authDomain: "proodosassignmenttracking.firebaseapp.com",
+      databaseURL: "https://proodosassignmenttracking.firebaseio.com",
+      projectId: "proodosassignmenttracking",
+      storageBucket: "proodosassignmenttracking.appspot.com",
+      messagingSenderId: "656213454825",
+      appId: "1:656213454825:web:e27e1d37f58fad37"
+    };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
+  
+
+  var database = firebase.database();
+console.log(database);
+  
+  $("#submitAssignmentLog").on("click", function(event) {
+      event.preventDefault();
+
+      getAssignmentValues();
+  })
+
+  function getAssignmentValues() {
+      
+      var taskId = $("#taskIdInput").val();
+      var taskName = $("#taskNameInput").val();
+      var description = $("#descInput").val();
+      var priority = $("#priorityInput").val();
+      var devId = $("#devIdAssign").val();
+       //var assignmentId = $("#assignmentId_input").val();
+      
+      
+      
+
+      var assignObj = {
+          // assignId : assignmentId,
+          devId : devId,
+          taskId : taskId,
+          priority : priority,
+          taskName : taskName,
+          description : description
+      }
+
+      writeUserData(assignObj);
+  };
+
+  function writeUserData(assignObj) {
+      database.ref().push({
+      //   assignId: assignObj.assignId,
+        taskId: assignObj.taskId,
+        devId : assignObj.devId,
+        priority :assignObj.priority,
+        taskName : assignObj.taskName,
+        description :assignObj. description
+
+      });
+      console.log("data has probably been sent");
+      console.log(assignObj);
+
+
+      function clearForm() {
+          $("#assignmentEntry").reset(); //This is the part didn't work for me
+      }
+ }
+});
    
-    var database = firebase.database();
-   
-   
-   
-   
-    $(“#submitAssignmentLog”).on(“click”, function(event) {
-        event.preventDefault();
-   
-        getAssignmentValues();
-    })
-   
-    function getAssignmentValues() {
-        var assignmentId = $(“#assignmentId_input”).val();
-        var taskId = $(“#taskId_input”).val();
-        var devId = $(“#developerId_input”).val();
-        var priority = $(“#priority_input”).val();
-        var taskName = $(“#taskName_input”).val();
-        var description = $(“#description_input”).val();
-   
-        var assignObj = {
-            assignId : assignmentId,
-            taskId : taskId,
-            devId : devId,
-            priority : priority,
-            taskName : taskName,
-            description : description
-        }
-   
-        writeUserData(assignObj);
-    };
-   
-    function writeUserData(assignObj) {
-        database.ref().push({
-          assignId: assignObj.assignId,
-          taskId: assignObj.taskId,
-          devId : assignObj.devId,
-          priority :assignObj.priority,
-          taskName : assignObj.taskName,
-          description :assignObj. description
-   
-        });
-        console.log(“data has probably been sent”)
-    }
-   });
